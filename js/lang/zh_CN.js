ea6705c0e30d1fc2fcec8790edd8bcf537f30699
Phoebe <wtsheryl@foxmail.com>
2019-01-07 15:53:17 +0800

    another

---
+++ js/lang/zh_CN.js
@@ -0,0 +1,9 @@
+//@ sourceURL=zh_CN.js
+$.lang = {
+    "company"							: "深圳般若计算机系统股份有限公司",
+    "request_error"                     : "请求异常",
+    "parameter_is_null"                 : "参数为空",
+    "system_error"                      : "系统异常,请联系我们"
+};
+
+
---
+++ js/main.js
@@ -0,0 +1,16 @@
+﻿jQuery(function($) {
+	$.renderHTML($("#header-content"), "page/nav", null);
+});
+$.extend({
+	renderHTML: function(ele, url, data) {
+		if(data == null) {
+			data = {};
+		}
+		data["lang"] = $.lang;
+		var html = new EJS({
+			"url": url
+		}).render(data);
+		$(ele).html(html);
+		html = null;
+	},
+});
